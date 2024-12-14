37d911412df4731f77ed9cea3c062b9fc5addcd2
Abir Banerjee <abirbanerjee793@gmail.com>
2024-12-14 15:50:50 +0530

    edit form fixed

--- client/src/pages/Editjob.tsx
+++ client/src/pages/Editjob.tsx
@@ -1,14 +1,35 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useSearchParams } from "react-router-dom"
-
+import { JobApplication } from '../types/types'
 const Editjob = () => {
-    const [searchParams, setSearchParams] = useSearchParams();
-    useEffect(()=>{
-        const id = searchParams.get('id');
-        
-        console.log(id)
+    const [searchParams] = useSearchParams();
+    const [JobDetails, setJobDetails] = useState<JobApplication>({
+        _id: "",
+        companyName: "",
+        createdAt: "",
+        dateApplied: "",
+        jobTitle: "",
+        notes: "",
+        status: "APPLIED",
+        updatedAt: "",
+        __v: 0,
+    })
+
+    useEffect(() => {
+        const fetchJob = async () => {
+            try {
+                const id = searchParams.get('id');
+                const result = await fetch(`http://localhost:8000/job/${id}`)
+                const data = await result.json();
+                console.log(data.message)
+                setJobDetails(data.message)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        fetchJob();
     }, [])
-    
+
     return (
         <>
             {/*
@@ -25,80 +46,86 @@ const Editjob = () => {
 
                 <form action="#" className="mx-auto mb-0 mt-8 max-w-md space-y-4">
                     <div>
-                        <label htmlFor="email" className="sr-only">Email</label>
+                        <label htmlFor="jobTitle" className="font-poppins font-light">Job Title</label>
 
                         <div className="relative">
                             <input
-                                type="email"
+                                type="text"
                                 className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
-                                placeholder="Enter email"
+                                placeholder="Enter Job Title"
+                                value={JobDetails.jobTitle}
+                                onChange={(e) => setJobDetails((prev) => ({
+                                    ...prev,
+                                    jobTitle: e.target.value
+                                }))}
                             />
-
-                            <span className="absolute inset-y-0 end-0 grid place-content-center px-4">
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    className="size-4 text-gray-400"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    stroke="currentColor"
-                                >
-                                    <path
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        strokeWidth="2"
-                                        d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
-                                    />
-                                </svg>
-                            </span>
                         </div>
                     </div>
 
                     <div>
-                        <label htmlFor="password" className="sr-only">Password</label>
+                        <label htmlFor="password" className="font-poppins font-light">Company Name</label>
 
                         <div className="relative">
                             <input
-                                type="password"
+                                type="text"
                                 className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
-                                placeholder="Enter password"
+                                placeholder="Enter Company Name"
+                                value={JobDetails.companyName}
+                                onChange={(e) => setJobDetails((prev) => ({
+                                    ...prev,
+                                    companyName: e.target.value
+                                }))}
                             />
 
-                            <span className="absolute inset-y-0 end-0 grid place-content-center px-4">
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    className="size-4 text-gray-400"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    stroke="currentColor"
-                                >
-                                    <path
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        strokeWidth="2"
-                                        d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                                    />
-                                    <path
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        strokeWidth="2"
-                                        d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
-                                    />
+
+                        </div>
+                    </div>
+
+                    <div>
+                        <label htmlFor="status" className="font-poppins font-light">Status</label>
+                        <div className="w-full max-w-sm min-w-[200px]">
+                            <div className="relative">
+                                <select
+
+                                    className="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded pl-3 pr-8 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-400 shadow-sm focus:shadow-md appearance-none cursor-pointer"
+                                    value={JobDetails.status}>
+                                    <option value="APPLIED">Applied</option>
+                                    <option value="INTERVIEW">Interview</option>
+                                    <option value="OFFERED">Offered</option>
+                                    <option value="REJECTED">Rejected</option>
+                                </select>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.2" stroke="currentColor" className="h-5 w-5 ml-1 absolute top-2.5 right-2.5 text-slate-700">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
                                 </svg>
-                            </span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div>
+                        <label htmlFor="notes" className="font-poppins font-light">Notes</label>
+
+                        <div className="relative">
+                            <input
+                                type="text"
+                                className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
+                                placeholder="Enter Company Name"
+                                value={JobDetails.notes}
+                                onChange={(e) => setJobDetails((prev) => ({
+                                    ...prev,
+                                    notes: e.target.value
+                                }))}
+                            />
+
+
                         </div>
                     </div>
 
                     <div className="flex items-center justify-between">
-                        <p className="text-sm text-gray-500">
-                            No account?
-                            <a className="underline" href="#">Sign up</a>
-                        </p>
-
                         <button
                             type="submit"
                             className="inline-block rounded-lg bg-blue-500 px-5 py-3 text-sm font-medium text-white"
                         >
-                            Sign in
+                            Save
                         </button>
                     </div>
                 </form>
--- server/routes/job.js
+++ server/routes/job.js
@@ -1,13 +1,30 @@
 const express = require('express');
-const { createJob,getAlljobs } = require('../controller/job');
+const { createJob, getAlljobs } = require('../controller/job');
+const JobsModel = require('../model/job');
 const router = express.Router();
 
 
-router.get('/', (req, res)=>{
+router.get('/', (req, res) => {
     console.log("dum dum");
-    return res.status(201).json({message : "All done", success : true})
+    return res.status(201).json({ message: "All done", success: true })
 })
 router.get('/alljobs', getAlljobs);
+
 router.post('/', createJob);
+
+router.get('/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        if (!id) return res.status(400).json({ message: "Invalid GET request", success: false })
+
+        const job = await JobsModel.findById(id);
+        if (!job) return res.status(400).json({ message: "No such job registered", success: false })
+
+
+        return res.status(201).json({ message: job, success: true });
+    } catch (error) {
+        return res.status(500).json({ message: "Internal Server Error", success: false });
+    }
+})
 
 module.exports = router
